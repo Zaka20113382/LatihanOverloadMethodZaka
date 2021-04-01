e18fc2ff1c40ad4c005994adcf7b04b6abdffc33
Zaka20113382 <zaka13zulkarnaen@gmail.com>
2021-04-01 17:10:28 +0700

    Add project files.

---
+++ overloadmethod/Numbers.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace overloadmethod
+{
+    class Numbers
+    {
+        public int FindMinimum(int number1, int number2)
+        {
+            int hasil;
+            if (number2 < number1)
+                hasil = number2;
+            else
+                hasil = number1;
+            return hasil;
+        }
+        public int FindMinimum(int number1, int number2, float number3)
+        {
+            int hasil;
+            if (number3 < number2)
+                hasil = (int)number3;
+            else
+            if (number2 < number1)
+                hasil = number2;
+            else
+                hasil = number1;
+            return hasil;
+        }
+        public int FindMaximum(int number1, int number2)
+        {
+            int hasil;
+            if (number2 > number1)
+                hasil = number2;
+            else
+                hasil = number1;
+            return hasil;
+        }
+        public int FindMaximum(int number1, int number2, float number3)
+        {
+            int hasil;
+            if (number3 > number2)
+                hasil = (int)number3;
+            else
+                if (number2 > number1)
+                hasil = number2;
+            else
+                hasil = number1;
+            return hasil;
+        }
+    }
+}
